b6d66381586c5d3a396da3edaa880b63dd9392bc
Hiroyuki Yamaoka <yamaoka@428lab.jp>
2013-04-27 18:03:57 +0900

    設定値の渡し方変更

--- src/Clover/Silex/ServiceProvider/TmhOAuthServiceProvider.php
+++ src/Clover/Silex/ServiceProvider/TmhOAuthServiceProvider.php
@@ -8,12 +8,15 @@ class TmhOAuthServiceProvider implements ServiceProviderInterface
 {
     public function register(Application $app)
     {
-        if (!isset($app['tmhoauth.config']['consumer_key'], $app['tmhoauth.config']['consumer_secret'])) {
+        if (!isset($app['tmhoauth.consumer_key'], $app['tmhoauth.consumer_secret'])) {
             throw new \RuntimeException('Config must include consumer_key and consumer_secret');
         }
 
         $app['tmhoauth'] = $app->share(function() use ($app) {
-            return new \tmhOAuth($app['tmhoauth.config']);
+            return new \tmhOAuth([
+                'consumer_key' => $app['tmhoauth.consumer_key'],
+                'consumer_secret' => $app['tmhoauth.consumer_secret'],
+            ]);
         });
     }
 
