f01e7af942e47b08bda18b331d46692efe3b1f37
Hiroyuki Yamaoka <yamaoka@428lab.jp>
2013-04-27 18:08:04 +0900

    チェックのタイミングはここじゃない

--- src/Clover/Silex/ServiceProvider/TmhOAuthServiceProvider.php
+++ src/Clover/Silex/ServiceProvider/TmhOAuthServiceProvider.php
@@ -8,10 +8,6 @@ class TmhOAuthServiceProvider implements ServiceProviderInterface
 {
     public function register(Application $app)
     {
-        if (!isset($app['tmhoauth.consumer_key'], $app['tmhoauth.consumer_secret'])) {
-            throw new \RuntimeException('Config must include consumer_key and consumer_secret');
-        }
-
         $app['tmhoauth'] = $app->share(function() use ($app) {
             return new \tmhOAuth([
                 'consumer_key' => $app['tmhoauth.consumer_key'],
