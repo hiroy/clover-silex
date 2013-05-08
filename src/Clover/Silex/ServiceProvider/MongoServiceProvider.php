eacb5da41df0988269953557f4145d0bcc3cf6d1
Hiroyuki Yamaoka <yamaoka@428lab.jp>
2013-05-08 16:42:10 +0900

    adds MongoServiceProvider to provide a MongoClient instance

---
+++ src/Clover/Silex/ServiceProvider/MongoServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+namespace Clover\Silex\ServiceProvider;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+class MongoServiceProvider implements ServiceProviderInterface
+{
+    public function register(Application $app)
+    {
+        if (!isset($app['mongodb.server'])) {
+            $app['mongodb.server'] = 'mongodb://localhost';
+        }
+        if (!isset($app['mongodb.options'])) {
+            $app['mongodb.options'] = [];
+        }
+        $app['mongodb.client'] = $app->share(function() use ($app) {
+            return new \MongoClient($app['mongodb.server'], $app['mongodb.options']);
+        });
+    }
+
+    public function boot()
+    {
+    }
+}
